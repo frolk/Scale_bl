deb00483913871b889d9dee8f65e4252ac476aeb
frolk <frolkin-stas@yandex.ru>
2016-01-17 14:56:27 +0500

    first

---
+++ Scale_bluetooth/Scale_bluetooth.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Scale_bluetooth.ino
+ Created:	1/17/2016 2:55:41 PM
+ Author:	Stas
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
